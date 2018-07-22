589aa6233f89b0a775088b48a76e5a10b4ea6d76
j0nk0 <aboutcontact@gmail.com>
2018-07-22 18:43:29 +0000

    rm space & add link & add extensions & add beep etc.

--- extract.sh
+++ extract.sh
@@ -1,19 +1,17 @@
 #!/bin/bash
-# function Extract for common file formats
-
-#Other archive tools:
-# bzip2, p7zip-full, unzip, xdg-utils, xz-utils, arj, binutils, cpio, lhasa, liblz4-tool, lrzip, lzip, lzop, ncompress, rar, unar, zip, zstd
-#Other archive types
-# xz,7z,lzma,arj,bzip2,gzip,rar,tar,zip,rpm,lz4,compress,zstd,lzip,lrzip,lzop,lha,deb.
+# Function to extract common file formats.
+#  https://github.com/j0nk0/Extract/blob/master/extract.sh
 
 SAVEIFS=$IFS
 IFS=$(echo -en "\n\b")
 
 function extract {
  if [ -z "$1" ]; then
-    # display usage if no parameters given
-    echo "Usage: extract <path/file_name>.<zip|rar|bz2|gz|tar|tbz2|tgz|Z|7z|xz|ex|tar.bz2|tar.gz|tar.xz|exe|apk|cpio>"
+    # Display usage if no parameters given.
+    echo "Usage: extract <path/file_name>.<zip|rar|bz2|gz|tar|tbz2|tgz|Z|7z|xz|ex|tar.bz2|tar.gz|tar.xz|arj|cab|chm|deb|dmg|iso|lzh|msi|rpm|udf|wim|xar|exe|apk|cpio>"
     echo "       extract <path/file_name_1.ext> [path/file_name_2.ext] [path/file_name_3.ext]"
+    echo ""    
+    echo "https://github.com/j0nk0/Extract/blob/master/extract.sh"
 #    return 1
  else
     for n in "$@"
@@ -29,21 +27,16 @@ function extract {
             *.gz)        gunzip ./"$n"      ;;
             *.zip)       unzip ./"$n"       ;;
             *.z|*.Z)     uncompress ./"$n"  ;;
-
             *.7z|*.arj|*.cab|*.chm|*.dmg|*.iso|*.lzh|*.msi|*.rpm|*.udf|*.wim|*.xar|*.exe|*.apk)
                          7z x ./"$n"        ;;
             *.deb)       7z x ./"$n"; tar -xvf ./"data.tar" ; rm data.tar ;;
             *.xz)        unxz ./"$n"        ;;
             *.exe)       cabextract ./"$n"  ;;
-
             *.cpio)      cpio -idv < ./"$n"  ;;
-            *)
-                         echo "Extract: '$n' - unknown archive method"
-                         return 1
-                         ;;
+            *)           echo "Extract: '$n' - Unknown archive method."; return 1 ;;
           esac
       else
-          echo "'$n' - file does not exist"
+          echo "'$n' - File does not exist"
           return 1
       fi
     done
@@ -51,4 +44,7 @@ fi
 }
 
 extract "$@"
+ ! which beep &>/dev/null || beep -f3000 -l400 -r3 -d1
+ echo "Done"
+
 IFS=$SAVEIFS
