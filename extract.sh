5cbbeaa926364f542e3af9a14961eb1354399957
j0nk0 <aboutcontact@gmail.com>
2018-07-22 16:59:17 +0000

    Update extract.sh

--- extract.sh
+++ extract.sh
@@ -1,19 +1,26 @@
 #!/bin/bash
 # function Extract for common file formats
 
+#Other archive tools:
+# bzip2, p7zip-full, unzip, xdg-utils, xz-utils, arj, binutils, cpio, lhasa, liblz4-tool, lrzip, lzip, lzop, ncompress, rar, unar, zip, zstd
+#Other archive types
+# xz,7z,lzma,arj,bzip2,gzip,rar,tar,zip,rpm,lz4,compress,zstd,lzip,lrzip,lzop,lha,deb.
+
 SAVEIFS=$IFS
 IFS=$(echo -en "\n\b")
 
 function extract {
  if [ -z "$1" ]; then
     # display usage if no parameters given
-    echo "Usage: extract <path/file_name>.<zip|rar|bz2|gz|tar|tbz2|tgz|Z|7z|xz|ex|tar.bz2|tar.gz|tar.xz>"
+    echo "Usage: extract <path/file_name>.<zip|rar|bz2|gz|tar|tbz2|tgz|Z|7z|xz|ex|tar.bz2|tar.gz|tar.xz|exe|apk|cpio>"
     echo "       extract <path/file_name_1.ext> [path/file_name_2.ext] [path/file_name_3.ext]"
+#    return 1
  else
     for n in "$@"
     do
       if [ -f "$n" ] ; then
           case "${n%,}" in
+            *.ace)       unace x ./"$n"     ;;
             *.tar.bz2|*.tar.gz|*.tar.xz|*.tbz2|*.tgz|*.txz|*.tar) 
                          tar xvf "$n"       ;;
             *.lzma)      unlzma ./"$n"      ;;
@@ -21,14 +28,17 @@ function extract {
             *.rar)       unrar x -ad ./"$n" ;;
             *.gz)        gunzip ./"$n"      ;;
             *.zip)       unzip ./"$n"       ;;
-            *.z)         uncompress ./"$n"  ;;
-            *.7z|*.arj|*.cab|*.chm|*.deb|*.dmg|*.iso|*.lzh|*.msi|*.rpm|*.udf|*.wim|*.xar)
+            *.z|*.Z)     uncompress ./"$n"  ;;
+
+            *.7z|*.arj|*.cab|*.chm|*.dmg|*.iso|*.lzh|*.msi|*.rpm|*.udf|*.wim|*.xar|*.exe|*.apk)
                          7z x ./"$n"        ;;
+            *.deb)       7z x ./"$n" & echo bb; tar -xvf ./"data.tar" ; rm data.tar ;;
             *.xz)        unxz ./"$n"        ;;
             *.exe)       cabextract ./"$n"  ;;
-            *.cpio)      cpio -id < ./"$n"  ;;
+
+            *.cpio)      cpio -idv < ./"$n"  ;;
             *)
-                         echo "extract: '$n' - unknown archive method"
+                         echo "Extract: '$n' - unknown archive method"
                          return 1
                          ;;
           esac
@@ -40,4 +50,5 @@ function extract {
 fi
 }
 
+extract "$@"
 IFS=$SAVEIFS
